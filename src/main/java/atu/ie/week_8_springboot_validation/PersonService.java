f917469e8c7e0342b888a7102ac4967e3953ad5d
Rakib <G00408644@atu.ie>
2023-11-21 16:10:45 +0000

    Final v4

--- src/main/java/atu/ie/week_8_springboot_validation/PersonService.java
+++ src/main/java/atu/ie/week_8_springboot_validation/PersonService.java
@@ -1,10 +1,13 @@
 package atu.ie.week_8_springboot_validation;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PersonService {
     public void savePerson(Person person){
         System.out.println("Person saved: " + person);
     }
-    public Person getPersonByEmployeeId(Person person){
+    public Person getPersonByEmployeeId(String employeeId){
         //fetch data from a database in future lab
         //For simplicity we return a dummy person
         return new Person();
